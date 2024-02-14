e9948d8b16c1106aa9f481283a83a787417070a5
kathatbat <katoria1234@icloud.com>
2024-02-13 18:18:41 -0600

    commit changes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,10 @@ module.exports = () => {
     ],
     module: {
       rules: [
-      
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
